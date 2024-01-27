4c549e286135aee7baf810527a8efb4c07de7d24
deeksha-0511 <154816082+deeksha-0511@users.noreply.github.com>
2024-01-27 15:12:11 +0000

    feach

--- api/promises.js
+++ api/promises.js
@@ -47,3 +47,45 @@ proFour.then(function(user){
      console.log(er);
 }).finally(()=> console.log('promise ho gya'))
 
+const proFive = new Promise(function(resolve,reject){
+    let error = false //true
+    if(!error){
+        resolve({userName:'js' , password: '8770'})
+    }
+    else {
+        reject ('ERROR: something went wrong js')
+    }
+})
+async function consumePromiseFive (){
+  try{
+    const pf = await proFive 
+    console.log(pf);
+  }catch(err) {
+     console.log(err);
+  }
+}
+consumePromiseFive()
+/*
+async function getallData (){
+   try {
+    const repons = await fetch('https://jsonplaceholder.typicode.com/users')
+   const data = await repons.json();
+   console.log(data);
+   } catch (error) {
+    console.log("ERROR");
+   }
+}
+getallData()*/
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response)=>{
+    return response.json()
+})
+.then((data)=>{
+    //console.log(data);
+   return data.name.json()
+}).then((name)=>{
+     
+      console.log(name);
+})
+.catch ((er)=>console.log(ERROR))
